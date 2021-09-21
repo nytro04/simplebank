c567f56478393005a0bf47c97d4850e3e0464e94
Francis Badasu <nytro04@gmail.com>
2021-09-21 03:52:11 +0000

    feat: add CRUD for entry

---
+++ db/query/entry.sql
@@ -0,0 +1,19 @@
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id,
+  amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE code = $1 LIMIT $1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+WHERE account_id = $1
+ORDER BY code
+LIMIT $2
+OFFSET $3;
--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -8,9 +8,15 @@ import (
 
 type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
+	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entry, error)
+	CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfer, error)
 	DeleteAccount(ctx context.Context, id int64) error
 	GetAccount(ctx context.Context, id int64) (Account, error)
+	GetEntry(ctx context.Context, limit int32) (Entry, error)
+	GetTranser(ctx context.Context, code int64) (Transfer, error)
 	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Account, error)
+	ListEntries(ctx context.Context, arg ListEntriesParams) ([]Entry, error)
+	ListTransfers(ctx context.Context, arg ListTransfersParams) ([]Transfer, error)
 	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
 }
 
